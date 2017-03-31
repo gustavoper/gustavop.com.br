178cb97d5f2f6cbb42f0cfd24f53ee88da109e4f
Gustavo Pereira <gustavoper@gmail.com>
2017-03-30 23:04:15 -0300

    Added tagcloud

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
     <!-- Theme CSS -->
     <link href="css/freelancer.min.css" rel="stylesheet">
 
+    <link href="css/tagcloud.css" rel="stylesheet">
+
     <!-- Custom Fonts -->
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
@@ -25,8 +27,8 @@
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
 </head>
@@ -34,170 +36,190 @@
 <body id="page-top" class="index">
 <div id="skipnav"><a href="#maincontent">Top</a></div>
 
-    <!-- Navigation -->
-    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="#page-top">Gustavo Pereira</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li class="page-scroll">
-                        <a href="#social-contact">Social/Contact</a>
-                    </li>
-                    <li>
-                        <a href="http://medium.com/@gustavoper">Blog</a>
-                    </li>
-                    <li class="page-scroll">
-                        <a href="#about">About</a>
-                    </li>
-
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+<!-- Navigation -->
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="#page-top">Gustavo Pereira</a>
         </div>
-        <!-- /.container-fluid -->
-    </nav>
 
-    <!-- Header -->
-    <header>
-        <div class="container" id="maincontent" tabindex="-1">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li class="page-scroll">
+                    <a href="#social-contact">Social/Contact</a>
+                </li>
+                <li>
+                    <a href="http://medium.com/@gustavoper">Blog</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="#about">About</a>
+                </li>
+
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+<!-- Header -->
+<header>
+    <div class="container" id="maincontent" tabindex="-1">
+        <div class="row">
+            <div class="col-lg-12">
+                <img class="img-responsive" src="img/mProfilesquare.png" alt="">
+                <div class="intro-text">
+                    <h1 class="name">Gustavo Pereira</h1>
+                    <hr class="star-light">
+                    <span class="skills">Senior PHP Developer, Geek and Drummer</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<!--skillset section -->
+<section class="skillset">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>My skills</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div id="htmltagcloud">
+                    <span id="0" class="wrd tagcloud0">
+                        <a href="#tagcloud">angular2<span class="freq">&nbsp;(2)</span></a></span> <span id="1" class="wrd tagcloud6"><a
+                            href="#tagcloud">git<span class="freq">&nbsp;(4)</span></a></span> <span id="2"
+                                                                                                     class="wrd tagcloud8"><a
+                            href="#tagcloud">javascript<span class="freq">&nbsp;(5)</span></a></span> <span id="3"
+                                                                                                            class="wrd tagcloud10"><a
+                            href="#tagcloud">linux<span class="freq">&nbsp;(6)</span></a></span> <span id="4"
+                                                                                                       class="wrd tagcloud0"><a
+                            href="#tagcloud">mongodb<span class="freq">&nbsp;(2)</span></a></span> <span id="5"
+                                                                                                         class="wrd tagcloud6"><a
+                            href="#tagcloud">mysql<span class="freq">&nbsp;(4)</span></a></span> <span id="6"
+                                                                                                       class="wrd tagcloud10"><a
+                            href="#tagcloud">php<span class="freq">&nbsp;(6)</span></a></span> <span id="7"
+                                                                                                     class="wrd tagcloud0"><a
+                            href="#tagcloud">prestashop<span class="freq">&nbsp;(2)</span></a></span> <span id="8"
+                                                                                                            class="wrd tagcloud3"><a
+                            href="#tagcloud">silex<span class="freq">&nbsp;(3)</span></a></span> <span id="9"
+                                                                                                       class="wrd tagcloud3"><a
+                            href="#tagcloud">slim<span class="freq">&nbsp;(3)</span></a></span> <span id="10"
+                                                                                                      class="wrd tagcloud3"><a
+                            href="#tagcloud">svn<span class="freq">&nbsp;(3)</span></a></span> <span id="11"
+                                                                                                     class="wrd tagcloud3"><a
+                            href="#tagcloud">zend-framework<span class="freq">&nbsp;(3)</span></a></span> <span
+                        id="12" class="wrd tagcloud3"><a href="#tagcloud">zfce-5.5<span
+                                class="freq">&nbsp;(3)</span></a></span></div>
+
+        </div>
+    </div>
+</section>
+
+
+
+<!-- About Section -->
+<section class="success" id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>About</h2>
+                <hr class="star-light">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-lg-offset-2">
+                <p>Hello there, my name is Gustavo! I was born in 1984, born and raised in Brazil. I am a software developer since 2005, working for several companies, helping them to achieve goals and make some real profit. I'm a passionate developer, so I just love what I do. </p>
+            </div>
+            <div class="col-lg-4">
+                <p>Nowadays I'm a PHP Developer, writing some code for food, coffee and fun. Always looking at new technologies and always improving my code, trying to learn and use code standards everyday.</p>
+
+            </div>
+            <div class="col-lg-8 col-lg-offset-2 text-center">
+                <a href="#" class="btn btn-lg btn-outline">
+                    <i class="fa fa-download"></i> Go to my Linkedin Profile
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- Footer -->
+<footer class="text-center">
+    <div class="footer-above">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col col-md-4">
+                    <h3>Location</h3>
+                    <p>Cubatão, São Paulo, Brazil<br>
+                        <em>Willing to relocate? Yes</em><br>
+                        <em>Can work remotely? Yes</em>
+                    </p>
+                </div>
+                <div class="footer-col col-md-8" id="social-contact">
+                    <h3>Where you can find me</h3>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="#" class="btn-social btn-outline"><span class="sr-only">Facebook</span><i class="fa fa-fw fa-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn-social btn-outline"><span class="sr-only">Google Plus</span><i class="fa fa-fw fa-google-plus"></i></a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn-social btn-outline"><span class="sr-only">Twitter</span><i class="fa fa-fw fa-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn-social btn-outline"><span class="sr-only">LinkedIn</span><i class="fa fa-fw fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-below">
+        <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <img class="img-responsive" src="img/mProfilesquare.png" alt="">
-                    <div class="intro-text">
-                        <h1 class="name">Gustavo Pereira</h1>
-                        <hr class="star-light">
-                        <span class="skills">Senior PHP Developer, Geek and Drummer</span>
-                    </div>
+                    Copyright &copy; Gustavo Pereira 2016 <br>
+                    Template by <a href="https://startbootstrap.com/template-overviews/freelancer/">StartBootstrap</a>
                 </div>
             </div>
         </div>
-    </header>
-
-    <!--skillset section -->
-    <section class="skillset">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>My skills</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-2"><img src="img/php-logo.png"></div>
-                <div class="col-lg-2"><img src="img/mysql-logo.png"></div>
-                <div class="col-lg-2"><img src="img/js-logo.png"></div>
-                <div class="col-lg-2"><img src="img/zf2-logo.png"></div>
-                <div class="col-lg-2"><img src="img/linux-logo.png"></div>
-                <div class="col-lg-2"><img src="img/zf-englogo.png"></div>
-
-            </div>
-        </div>
-    </section>
-
-
-
-    <!-- About Section -->
-    <section class="success" id="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>About</h2>
-                    <hr class="star-light">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-lg-offset-2">
-                    <p>Hello there, my name is Gustavo! I was born in 1984, born and raised in Brazil. I am a software developer since 2005, working for several companies, helping them to achieve goals and make some real profit. I'm a passionate developer, so I just love what I do. </p>
-                </div>
-                <div class="col-lg-4">
-                    <p>Nowadays I'm a PHP Developer, writing some code for food, coffee and fun. Always looking at new technologies and always improving my code, trying to learn and use code standards everyday.</p>
-
-                </div>
-                <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <a href="#" class="btn btn-lg btn-outline">
-                        <i class="fa fa-download"></i> Go to my Linkedin Profile
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-
-    <!-- Footer -->
-    <footer class="text-center">
-        <div class="footer-above">
-            <div class="container">
-                <div class="row">
-                    <div class="footer-col col-md-4">
-                        <h3>Location</h3>
-                        <p>Cubatão, São Paulo, Brazil<br>
-                           <em>Willing to relocate? Yes</em><br>
-                            <em>Can work remotely? Yes</em>
-                        </p>
-                    </div>
-                    <div class="footer-col col-md-8" id="social-contact">
-                        <h3>Where you can find me</h3>
-                        <ul class="list-inline">
-                            <li>
-                                <a href="#" class="btn-social btn-outline"><span class="sr-only">Facebook</span><i class="fa fa-fw fa-facebook"></i></a>
-                            </li>
-                            <li>
-                                <a href="#" class="btn-social btn-outline"><span class="sr-only">Google Plus</span><i class="fa fa-fw fa-google-plus"></i></a>
-                            </li>
-                            <li>
-                                <a href="#" class="btn-social btn-outline"><span class="sr-only">Twitter</span><i class="fa fa-fw fa-twitter"></i></a>
-                            </li>
-                            <li>
-                                <a href="#" class="btn-social btn-outline"><span class="sr-only">LinkedIn</span><i class="fa fa-fw fa-linkedin"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-below">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        Copyright &copy; Gustavo Pereira 2016 <br>
-                        Template by <a href="https://startbootstrap.com/template-overviews/freelancer/">StartBootstrap</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
-    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
-        <a class="btn btn-primary" href="#page-top">
-            <i class="fa fa-chevron-up"></i>
-        </a>
     </div>
+</footer>
 
-    <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+<!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+<div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+    <a class="btn btn-primary" href="#page-top">
+        <i class="fa fa-chevron-up"></i>
+    </a>
+</div>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- jQuery -->
+<script src="vendor/jquery/jquery.min.js"></script>
 
-    <!-- Plugin JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 
 
 
-    <!-- Theme JavaScript -->
-    <script src="js/freelancer.min.js"></script>
+<!-- Theme JavaScript -->
+<script src="js/freelancer.min.js"></script>
 
 </body>
 
